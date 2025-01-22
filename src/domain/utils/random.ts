8a0337410357587e7937b65b28e369bfd78a276f
clement_obert <clement_obert@ext.connect-tech.sncf>
2025-01-22 17:19:22 +0100

    Clean package.json

--- src/domain/utils/random.ts
+++ src/domain/utils/random.ts
@@ -1,7 +1,4 @@
 import * as d3 from "d3-random";
-import {Delaunay} from "d3-delaunay";
-import {polygonCentroid} from "d3-polygon";
-import Polygon = Delaunay.Polygon;
 
 const seed = Math.random()
 const source = d3.randomLcg(seed)
@@ -18,26 +15,4 @@ export const rand2d = (xMin: number, yMin: number, xMax: number, yMax: number) =
     const randX = randomInt(xMin, xMax);
     const randY = randomInt(yMin, yMax);
     return () => [randX(), randY()];
-}
-
-export const randomInPolygon = (
-    polygon: Polygon,
-    randomizer: (vMin: number, vMax: number, center: number) => () => number
-) => {
-    const [cx, cy] = polygonCentroid(polygon);
-    const vectorX = polygon.map(([x, _]) => x)
-    const randomX = randomizer(Math.min(...vectorX), Math.max(...vectorX), cx);
-
-    return () => {
-        const x = randomX();
-        const vectorY = polygon.slice(1)
-            .filter((_, i, points) => {
-                const d = (x - polygon[i][0]) / (points[i][0] - polygon[i][0]);
-                return d > 0 && d < 1
-            })
-            .map(([_, y]) => y);
-        const randomY = randomizer(Math.min(...vectorY), Math.max(...vectorY), cy);
-
-        return [x, randomY()];
-    }
 }
--- src/view/pixi/ui.ts
+++ src/view/pixi/ui.ts
@@ -1,16 +1,21 @@
-import {Container, ContainerChild, Text, TextStyle} from "pixi.js";
+import {Graphics, Rectangle, Text, TextStyle} from "pixi.js";
+import {List} from "@pixi/ui"
 
-export class UserInterface extends Container<ContainerChild> {
-    private style = new TextStyle({fill: "black"});
-    private biome: Text
+export class UserInterface extends Graphics {
+    private style = new TextStyle({fill: "black", align: "center", wordWrap: true, wordWrapWidth: 200});
+    private readonly biomeView: Text
 
     constructor() {
         super();
-        this.biome = new Text({style: this.style});
-        this.addChild(this.biome);
+        this.roundRect(0, 0, 200, 30, 10).fill("white");
+        const list = new List({});
+
+        this.biomeView = new Text({style: this.style, boundsArea: new Rectangle(0, 0, 160, 30)});
+        this.addChild(this.biomeView);
+        this.biomeView.x = 200 - this.biomeView.boundsArea.width;
     }
 
     updateCurrentBiome(biome: string) {
-        this.biome.text = biome;
+        this.biomeView.text = biome;
     }
 }
